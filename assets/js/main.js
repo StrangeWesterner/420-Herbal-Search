bc5887130248d7a59dfae632ddcf701baeca326a
Strangebrewer <BKAShambala@gmail.com>
2018-04-06 01:53:39 -0600

    make responsive and finalize styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,6 +73,18 @@
 
 var currentPage = 1;
 
+function getRandomParams() {
+  var randomNum = Math.floor(Math.random() * Math.floor(2));  
+  if (randomNum === 0) {
+    return strainsArrayOne[Math.floor(Math.random() * Math.floor(strainsArrayOne.length))];
+    console.log(randomTerm);
+  }
+  else if (randomNum === 1) {
+    return strainsArrayTwo[Math.floor(Math.random() * Math.floor(strainsArrayTwo.length))];
+    console.log(randomTerm);
+  }
+}
+
 function reportSearch(search, page) {
   $.ajax({
     method: "GET",
@@ -80,35 +92,56 @@ function reportSearch(search, page) {
   }).then(function (response) {
     console.log(response);
     var info = response.data;
-    for (let i = 0; i < info.length; i++) {
-      const element = info[i];
-      let reportDiv = $("<div>");
-      let name = $(`<h3>${element.name}</h3>`);
-      let imageLink = $(`<a href=${element.image}>See Image</a><br>`);
-      let infoLink = $(`<a href=${element.url}>more info</a>`);
-      reportDiv.addClass("report-response");
-      imageLink.addClass("report-img");
-      infoLink.attr("target", "_blank");
-      name.addClass("report-name");
-      infoLink.addClass("report-link");
-      reportDiv.append(name);
-      reportDiv.append(imageLink);
-      reportDiv.append(infoLink);
-      $("#search-results").append(reportDiv);
+    if (info.length === 0) {
+      var randomTerm = getRandomParams();
+      reportSearch(randomTerm, currentPage);
     }
-    if (response.meta.pagination.total_pages > currentPage) {
-      currentPage++;
-      var moreBtn = $("<button>Load More Results</button>");
-      moreBtn.addClass("more-results");
-      moreBtn.attr("page", currentPage);
-      moreBtn.attr("search", search);
-      $("#search-results").append(moreBtn);
+    else {
+      for (let i = 0; i < info.length; i++) {
+        const element = info[i];
+        let reportDiv = $("<div>");
+        let name = $(`<h3>${element.name}</h3>`);
+        let noImage = $("<p>( no image available )</p>");
+        let imageLink = $(`<a href=${element.image}>See Image</a><br>`);
+        let genetics = $(`<h4>Genetics: ${element.genetics.names}</h4>`);
+        let noGen = $("<h4>Genetics: Unknown</h4>");
+        let seedCo = $(`<h4>Seed Company: ${element.seedCompany.name}</h4>`);
+        let infoLink = $(`<a href=${element.url}>more info</a>`);
+        reportDiv.addClass("report-response");
+        imageLink.addClass("info report-img");
+        noImage.addClass("info no-img");
+        genetics.addClass("info report-data");
+        noGen.addClass("info report-data");
+        seedCo.addClass("info report-data");
+        infoLink.attr("target", "_blank");
+        name.addClass("report-name");
+        infoLink.addClass("info report-link");
+        reportDiv.append(name);
+
+        if (element.image.includes("no_image")) reportDiv.append(noImage);
+        else reportDiv.append(imageLink);
+
+        if (element.genetics.names === false) reportDiv.append(noGen);
+        else reportDiv.append(genetics);
+
+        reportDiv.append(seedCo);
+        reportDiv.append(infoLink);
+        $("#search-results").append(reportDiv);
+      }
+      if (response.meta.pagination.total_pages > currentPage) {
+        currentPage++;
+        var moreBtn = $("<button>Load More Results</button>");
+        moreBtn.attr("id", "more-results");
+        moreBtn.attr("page", currentPage);
+        moreBtn.attr("search", search);
+        $("#search-results").append(moreBtn);
+      }
     }
   });
 }
 
-$("#search-results").on("click", ".more-results", function () {
-  console.log("hi!");
+$("#search-results").on("click", "#more-results", function () {
+  console.log("Hi");
   var newSearch = $(this).attr("search");
   var newPage = $(this).attr("page");
   console.log(newPage);
@@ -129,16 +162,7 @@ $("#reports-random").on("click", function (event) {
   event.preventDefault();
   $("#search-results").html("");
   currentPage = 1;
-  var randomNum = Math.floor(Math.random() * Math.floor(2));
-  var randomTerm = "";
-  if (randomNum === 0) {
-    randomTerm = strainsArrayOne[Math.floor(Math.random() * Math.floor(strainsArrayOne.length))];
-    console.log(randomTerm);
-  }
-  else if (randomNum === 1) {
-    randomTerm = strainsArrayTwo[Math.floor(Math.random() * Math.floor(strainsArrayTwo.length))];
-    console.log(randomTerm);
-  }
+  var randomTerm = getRandomParams();
   reportSearch(randomTerm, currentPage);
 });
 
@@ -153,13 +177,13 @@ function strainSearch(search) {
       const element = result[i];
       let strainDiv = $("<div>");
       let name = $(`<h3>${element.name}</h3>`);
-      let race = $(`<h4>${element.race}</h4>`);
+      let race = $(`<h4>Species: ${element.race}</h4>`);
       let desc = $(`<p>${element.desc}</p>`);
       let noDesc = $("<p>No description available.</p>");
       strainDiv.addClass("strain-response");
       name.addClass("strain-name");
-      race.addClass("strain-race");
-      desc.addClass("strain-desc");
+      race.addClass("info strain-race");
+      desc.addClass("info strain-desc");
       strainDiv.append(name);
       strainDiv.append(race);
       if (element.desc === null) strainDiv.append(noDesc);
@@ -177,7 +201,7 @@ $("#strains-btn").on("click", function (event) {
   if (strainsArrayOne.toString().toLowerCase().includes(searchTerm) || strainsArrayTwo.toString().toLowerCase().includes(searchTerm)) {
     strainSearch(searchTerm);
   } else {
-    $("#search-results").html("We have no listing of a strain by that name. Please try again.");
+    $("#search-results").html("<p class='no-strain'>We have no listing of a strain by that name. Please try again.</p>");
   }
 });
 
@@ -195,4 +219,9 @@ $("#strains-random").on("click", function (event) {
     console.log(randomTerm);
   }
   strainSearch(randomTerm);
+});
+
+$('#search-results').magnificPopup({
+  delegate: '.report-img',
+  type: 'image'
 });
